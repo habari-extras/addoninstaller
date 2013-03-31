feeee47a57e60fb9be5be854a060787e5839704e
Konzertheld <github@konzertheld.de>
2013-03-31 01:46:55 +0100

    Initial commit. Retrieve addon list and display a notice for every addon.

---
+++ addoninstaller.plugin.php
@@ -0,0 +1,45 @@
+<?php
+class AddonInstaller extends Plugin
+{
+	public function action_init()
+	{
+		$this->add_rule('"retrieve_addonlist"', 'retrieve_addonlist');
+		$this->add_rule('"install_addons"', 'install_addons');
+	}
+	
+	public function theme_route_retrieve_addonlist($theme, $params)
+	{
+		if(isset($_POST['payload']) && !empty($_POST['payload'])) {
+			$data = $_POST['payload'];
+			Cache::set('install_addons', $data);
+			echo json_encode(array("status" => "OK"));
+		}
+	}
+	
+	public function theme_route_install_addons($theme, $params)
+	{
+		if(!User::identify()->loggedin) {
+			Session::notice('You have to login to install addons');
+			Utils::redirect(Site::get_url('login'));
+		}
+		else {
+			// Start processing the addons we were passed
+			$data = Cache::get('install_addons');
+			foreach($data as $addon) {
+				Session::notice("You have " . $addon["name"] . " waiting for install");
+			}
+		}
+		Utils::redirect(Site::get_url("admin"));
+	}
+	
+	public function filter_login_redirect_dest($login_dest, $user, $login_session)
+	{
+		$data = Cache::get('install_addons');
+		if(isset($data) && !empty($data)) {
+			// Only redirect when we caused it
+			$login_dest = Site::get_url('habari') . '/install_addons';
+		}
+		return $login_dest;
+	}
+}
+?>
