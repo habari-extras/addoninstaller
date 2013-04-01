ee944e665c151fda469aebe9a03c8245a2dd08c1
Konzertheld <christian.gredig@gmail.com>
2013-04-01 21:52:08 +0200

    Make installing addons actually work. At least they are now downloaded correctly from the catalog and moved to the correct directory so they get available for activation. That's awesome and fixes habari-extras/addon_catalog#31. Yay! Also, the payload handling has been improved and a bit styling as been added.

--- addoninstaller.plugin.php
+++ addoninstaller.plugin.php
@@ -11,6 +11,13 @@ class AddonInstaller extends Plugin
 		'core' => 'Core',
 	);
 	
+	/** @var array $_types Subdirectories for default addon types */
+	protected $_type_subdirs = array(
+		'theme' => 'themes',
+		'plugin' => 'plugins',
+		'locale' => 'locale',
+	);
+
 	public function action_init()
 	{
 		$this->add_template( 'addon_preview', dirname(__FILE__) . '/templates/addon_preview.php' );
@@ -23,7 +30,10 @@ class AddonInstaller extends Plugin
 	public function theme_route_install_addons($theme, $params)
 	{
 		if(isset($_POST['payload']) && !empty($_POST['payload'])) {
-			Session::add_to_set('install_addons', json_decode($_POST->raw('payload')));
+			$payload = json_decode($_POST->raw('payload'));
+			foreach($payload as $addondata) {
+				Session::add_to_set('install_addons', $addondata, $addondata->slug);
+			}
 		}
 		
 		if(!User::identify()->loggedin) {
@@ -100,23 +110,63 @@ class AddonInstaller extends Plugin
 			Session::error(_t("You have no writable temporary directory. Your webserver needs to have write access to either your server's tempdir or Habari's user directory."));
 		}
 		
+		$data = Session::get_set('install_addons', false);
+		
 		// Check if we need to do something before displaying the list
 		$action = (isset($handler->handler_vars["action"])) ? $handler->handler_vars["action"] : "";
 		switch($action)
 		{
 			case "install":
+				$addon = $handler->handler_vars["addon"];
+				if(!array_key_exists($addon, $data)) {
+					// What did you pass us there?
+					Session::error("There was a problem handling the addon identifier. Maybe you used a wrong link?");
+					break;
+				}
+				
+				// Get the addon!
+				$request = new RemoteRequest($data[$addon]->download_url);
+				$request->execute();
+				file_put_contents($tmpdir . "/download.zip", $request->get_response_body());
+				
+				// Extract it to the real directory
+				$zip = new \ZipArchive();
+				$zipstatus = $zip->open($tmpdir . "/download.zip");
+				if($zipstatus === true) {
+					$addonpath = Site::get_dir("user") . '/' . $this->_type_subdirs[$data[$addon]->type] . '/' . $data[$addon]->name;
+					if(!is_dir($addonpath)) {
+						mkdir($addonpath);
+					}
+					if($zip->extractTo($addonpath)) {
+						$this->remove_from_set($addon);
+						Session::notice("Plugin now available for activation");
+						Utils::redirect(URL::get( 'admin', array("page" => "addon_preview")));
+					}
+					else {
+						Session::error("There was a problem extracting the zip file. Maybe your user directory is no writable?");
+					}
+					$zip->close();
+				}
+				else {
+					switch($zipstatus) {
+						case \ZipArchive::ER_NOZIP:
+							Session::error(_t("There was a problem retrieving the zip file"));
+							break;
+						default:
+							Session::error(_t("There was a problem opening the zip file"));
+							break;
+					}
+				}
 				break;
 		}
 		
 		// Pass the addon list to the theme
-		$payload = Session::get_set('install_addons', false);
-		$data = $payload[0];
 		$addons = array();
 		foreach($data as $addon) {
 			// Insert other checks here
 			$addon->habari_compatible = (version_compare(Version::get_habariversion(), $addon->habari_version) == -1) ? false : true;
-			if($addon->habari_compatible) {
-				$actions = array("install" => URL::get( 'admin', array("page" => "addon_preview", "action" => "install", "addon" => "")));
+			if($addon->habari_compatible && $tmpdir) {
+				$actions = array("install" => URL::get( 'admin', array("page" => "addon_preview", "action" => "install", "addon" => $addon->slug)));
 				$addon->actions = $actions;
 			}
 			$addons[$addon->type][] = $addon;
@@ -147,6 +197,20 @@ class AddonInstaller extends Plugin
 			}
 		}
 		else return false;
+	}
+	
+	/**
+	 * Helper function to easily get rid of a single entry in the Session
+	 */
+	function remove_from_set($slug)
+	{
+		$data = Session::get_set("install_addons");
+		foreach($data as $index => $entry) {
+			if($index == $slug) {
+				continue;
+			}
+			$Session::add_to_set("install_addons", $entry, $index);
+		}
 	}
 }
 ?>
--- templates/addon.php
+++ templates/addon.php
@@ -3,18 +3,20 @@
 <div class="item plugin clear">
 	<div class="head">
 		<a href="<?php echo $addon->download_url; ?>" class="addon"><?php echo $addon->name; ?> <span class="version"><?php echo $addon->version; ?></span></a>
+		<p class="description"><?php echo $addon->description; ?></p>
 	</div>
-	
-	<span class="dim"><?php _e('by'); ?></span>
 
-	<?php if(isset($addon->author)) {
+	<?php if(isset($addon->author)): ?>
+		<span class="dim"><?php _e('by'); ?></span>
+		<?php
 		$authors = array();
 		foreach ( $addon->author as $author ) {
 			$authors[] = isset( $author['url'] ) ? '<a href="' . $author['url'] . '">' . $author . '</a>' : $author;
 		}
 		// @locale The string used between the last two items in the list of authors of an addon on the "Install addons page" (one, two, three *and* four).
 		echo Format::and_list( $authors, '<span class="dim">, </span>', '<span class="dim">' . _t( ' and ' ) . '</span>');
-	} ?>
+		endif;
+	?>
 	
 	<?php if ( isset($addon->help) ): ?>
 		<a class="help" href="<?php echo $addon->help_url; ?>">?</a>
