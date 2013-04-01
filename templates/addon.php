656af741466d2de60560f82ef2b65a7c8f7af563
Konzertheld <christian.gredig@gmail.com>
2013-04-01 06:13:40 +0200

    Crap, forgot to add the templates in the previous commit. Here you go.

---
+++ templates/addon.php
@@ -0,0 +1,5 @@
+<div class="item plugin clear">
+	<div class="head">
+		<a href="<?php echo $addon->download_url; ?>" class="addon"><?php echo $addon->name; ?> <span class="version"><?php echo $addon->version; ?></span></a>
+	</div>
+</div>
---
+++ templates/addon_preview.php
@@ -0,0 +1,21 @@
+<?php namespace Habari; ?>
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
+<?php $theme->display('header'); ?>
+
+<?php foreach($addon_types as $type => $type_display): ?>
+	<?php if(isset($addons[$type]) && count($addons[$type])): ?>
+		<div class="container plugins" id="available_<?= $type ?>_list">
+
+			<h2><?php _e('%s available for installation', array($type_display)); ?></h2>
+
+			<?php
+			foreach ( $addons[$type] as $addon ) {
+				$theme->addon = $addon;
+				$theme->display('addon');
+			}
+			?>
+
+		</div>
+	<?php endif; ?>
+<?php endforeach; ?>
+<?php $theme->display('footer'); ?>
